11e63fc47e0380e7b25d03dc664982c6da1051f7
Md. Osman Goni Khan <khan15-5175@diu.edu.bd>
2025-09-21 09:23:19 +0600

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'New project Flutter',
+      home: Home(),
+
+
+
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Flutter App'),
+        centerTitle: true,
+        backgroundColor: Colors.cyan,
+      ),
+      body: Column(
+        children: [
+          Text('This is Column',style: TextStyle(
+            fontSize: 30,
+            color: Colors.blue
+          ),),
+          Text('This is Column',style: TextStyle(
+              fontSize: 30,
+              color: Colors.blue
+          ),),
+          Text('This is Column',style: TextStyle(
+              fontSize: 30,
+              color: Colors.blue
+          ),),
+          Text('This is Column',style: TextStyle(
+              fontSize: 30,
+              color: Colors.blue
+          ),),
+
+        ],
+      ),
+    );
+  }
+}
