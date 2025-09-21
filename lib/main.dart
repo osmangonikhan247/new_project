10f2508587097f8126a448fdaef4c5b7f1eb0c98
Md. Osman Goni Khan <khan15-5175@diu.edu.bd>
2025-09-21 22:13:13 +0600

    new home is view page

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'New project Flutter', home: Home());
+        title: 'New project Flutter',
+        // home: Home()
+      home: view(),
+    );
   }
 }
 
@@ -289,3 +292,39 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+class view extends StatelessWidget {
+  const view({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('View Page'),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'This is Column',
+            style: TextStyle(fontSize: 30, color: Colors.blue),
+          ),
+          Text(
+            'This is Column',
+            style: TextStyle(fontSize: 30, color: Colors.blue),
+          ),
+          Text(
+            'This is Column',
+            style: TextStyle(fontSize: 30, color: Colors.blue),
+          ), Text(
+            'This is Column',
+            style: TextStyle(fontSize: 30, color: Colors.blue),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
